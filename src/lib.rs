43d95c613628230589199307c92bc9036fdd2226
Andrei Silviu Dragnea <andreidragnea.dev@gmail.com>
2024-04-15 13:01:32 +0200

    Add notify_transaction log

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use solana_geyser_plugin_interface::geyser_plugin_interface::GeyserPlugin;
+use solana_geyser_plugin_interface::geyser_plugin_interface::Result;
+use solana_geyser_plugin_interface::geyser_plugin_interface::{
+    GeyserPlugin, ReplicaTransactionInfoVersions,
+};
+use solana_sdk::clock::Slot;
 use std::fmt::Debug;
 
 #[derive(Debug)]
@@ -7,6 +11,25 @@ struct GeyserPluginImpl;
 impl GeyserPlugin for GeyserPluginImpl {
     fn name(&self) -> &'static str {
         "GeyserPluginImpl"
+    }
+
+    fn notify_transaction(
+        &self,
+        transaction: ReplicaTransactionInfoVersions,
+        slot: Slot,
+    ) -> Result<()> {
+        println!(
+            "notify_transaction(slot={slot}, transaction={})",
+            match transaction {
+                ReplicaTransactionInfoVersions::V0_0_1(replica_transaction_info) => {
+                    format!("{:#?}", replica_transaction_info)
+                }
+                ReplicaTransactionInfoVersions::V0_0_2(replica_transaction_info_v2) => {
+                    format!("{:#?}", replica_transaction_info_v2)
+                }
+            }
+        );
+        Ok(())
     }
 }
 
